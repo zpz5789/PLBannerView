9bf6e1070e5f79858c30dd228d97cd2ff17e08e2
zpz <zpz@zpzdemacbookpro1.local>
2015-11-28 11:04:20 +0800

    Initial Commit

---
+++ PLBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PLBannerView
+//
+//  Created by zpz on 15/11/28.
+//  Copyright © 2015年 zpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PLBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PLBannerView
+//
+//  Created by zpz on 15/11/28.
+//  Copyright © 2015年 zpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
